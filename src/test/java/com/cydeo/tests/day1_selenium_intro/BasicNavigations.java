ae21ec66c6c649c9037bb095c2cc6bff8d6ab760
FeiZhou <sophia08241@gmail.com>
2022-02-01 16:39:05 -0500

    day1 basic navigations

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -16,6 +16,11 @@ public class BasicNavigations {
         // This is the line opening an empty browser
         WebDriver driver = new ChromeDriver();
 
+        // this line will maximize the broswer size
+        driver.manage().window().maximize();
+
+       // driver.manage().window().fullscreen();  //this one only works for Mac
+
         //3- Go to "https:// www.tesla.com"
         driver.get("https://www.tesla.com");
 
@@ -60,6 +65,12 @@ public class BasicNavigations {
 
         currentURL = driver.getCurrentUrl();
         System.out.println("currentURL = " + currentURL);
+
+        //this will close the currently opened window
+        driver.close();
+
+        //this will close all the opened windows
+        driver.quit();
 
 
     }
