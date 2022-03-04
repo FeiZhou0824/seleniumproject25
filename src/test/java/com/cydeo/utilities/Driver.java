a0b7bd44306fc23d21174adc558c31217ebd93da
FeiZhou <sophia08241@gmail.com>
2022-03-04 15:31:24 -0500

    day 9 java faker, testbase and driver

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -1,9 +1,17 @@
 package com.cydeo.utilities;
 
+import org.openqa.selenium.WebDriver;
+
 public class Driver {
-
-
-
+    /*
+    Creating a private constructor, so we are closing access to the object of this class from outside of the class
+     */
+    private Driver(){}
+    /*
+    We make WebDriver private, because we wnt to close access from outside the class.
+    We make it static because we will use it in a static method.
+     */
+    private static WebDriver driver;
 
 
 }
