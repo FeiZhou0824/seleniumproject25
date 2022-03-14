6717caaa8af2b6df59c3a8e536e347770e409e50
FeiZhou <sophia08241@gmail.com>
2022-03-13 21:59:05 -0400

    day 12 Library page

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -3,12 +3,19 @@ package com.cydeo.tests.day12_pom_design_explicit_wait;
 import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 
 public class POMPractices {
     LibraryLoginPage libraryLoginPage;
+
+    @BeforeMethod
+    public void setupMethod(){
+        Driver.getDriver().get(" https://library1.cydeo.com/");
+        libraryLoginPage = new LibraryLoginPage();
+    }
 
 
     @Test
@@ -18,10 +25,10 @@ public class POMPractices {
 //        1- Open a chrome browser
 //        2- Go to: https://library1.cydeo.com/
 
-        Driver.getDriver().get(" https://library1.cydeo.com/");
+
 
 //        3- Do not enter any information
-        libraryLoginPage = new LibraryLoginPage();
+
 
 //        4- Click to “Sign in” button
         libraryLoginPage.signInButton.click();
@@ -29,6 +36,8 @@ public class POMPractices {
 //        5- Verify expected error is displayed:
 //        Expected: This field is required.
         Assert.assertTrue(libraryLoginPage.filedRequiredErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
 //
 //        NOTE: FOLLOW POM DESIGN PATTERN
 
@@ -39,10 +48,10 @@ public class POMPractices {
         //1- Open a chrome browser
         //2- Go to: https://library1.cydeo.com
 
-        Driver.getDriver().get("https://library1.cydeo.com");
+
         //            3- Enter invalid email format
 
-        libraryLoginPage = new LibraryLoginPage();
+
 
         libraryLoginPage.inputUsername.sendKeys("dfgdrsdfghj");
         libraryLoginPage.signInButton.click();
@@ -51,6 +60,8 @@ public class POMPractices {
         //4- Verify expected error is displayed:
        //    Expected: Please enter a valid email address.
         Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
        //
        //    NOTE: FOLLOW POM DESIGN PATTERN
 
@@ -63,16 +74,18 @@ public class POMPractices {
 //        TC #3: Library negative login
 //        1- Open a chrome browser
 //        2- Go to: https://library1.cydeo.com/
-        Driver.getDriver().get("https://library1.cydeo.com/");
 
 //        3- Enter incorrect username or incorrect password
-       libraryLoginPage = new LibraryLoginPage();
+
         libraryLoginPage.inputUsername.sendKeys("dahjghjkaghkj@hg.com");
         libraryLoginPage.signInButton.click();
 
 //        4- Verify title expected error is displayed:
 //        Expected: Sorry, Wrong Email or Password
         Assert.assertTrue(libraryLoginPage.wrongEmailOrPasswordErrorMessage.isDisplayed());
+
+
+        Driver.closeDriver();
 //
 //        NOTE: FOLLOW POM DESIGN PATTERN
     }
---
+++ src/test/java/com/cydeo/tests/homeWork/CheckBoxes.java
@@ -0,0 +1,6 @@
+package com.cydeo.tests.homeWork;
+
+public class CheckBoxes {
+
+
+}
